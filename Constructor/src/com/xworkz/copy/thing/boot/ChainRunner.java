333c4bb78a9c0b8ddcd966afbeedcd0e72767d49
Anusha KM <113164399+Anushaakm@users.noreply.github.com>
2022-10-17 08:35:55 +0530

    Constructor chaining

--- Constructor/src/com/xworkz/copy/thing/boot/ChainRunner.java
+++ Constructor/src/com/xworkz/copy/thing/boot/ChainRunner.java
@@ -9,32 +9,32 @@ public class ChainRunner {
 		Chain chain=new Chain();
 		System.out.println(chain.type);
 				
-				Chain chain0=new Chain("Gold Chain");
+				Chain chain0=new Chain("ChainA");
 				System.out.println(chain0.type);
 				
-				Chain chain1=new Chain("Gold Chain",100000);
+				Chain chain1=new Chain("ChainB",20000);
 				System.out.println(chain1.type);
 				System.out.println(chain1.price);
 				
-				Chain chain2=new Chain("Gold Chain",100000,14);
+				Chain chain2=new Chain("ChainC",20000,14);
 				System.out.println(chain2.type);
 				System.out.println(chain2.price);
 				System.out.println(chain2.length);
 				
-				Chain chain3=new Chain("Gold Chain",100000,14,"24 Carrot Gold");
+				Chain chain3=new Chain("ChainD",2000,14,"24 Carrot Gold");
 				System.out.println(chain3.type);
 				System.out.println(chain3.price);
 				System.out.println(chain3.length);
 				System.out.println(chain3.material);
 				
-				Chain chain4=new Chain("Gold Chain",100000,14,"24 Carrot Gold",20);
+				Chain chain4=new Chain("ChainE",2000,14,"24 Carrot Gold",20);
 				System.out.println(chain4.type);
 				System.out.println(chain4.price);
 				System.out.println(chain4.length);
 				System.out.println(chain4.material);
 				System.out.println(chain4.weight);
 				
-				Chain chain5=new Chain("Gold Chain",100000,14,"24 Carrot Gold",20, false);
+				Chain chain5=new Chain("ChainF",2000,14,"24 Carrot Gold",20, false);
 				System.out.println(chain5.type);
 				System.out.println(chain5.price);
 				System.out.println(chain5.length);
@@ -42,7 +42,7 @@ public class ChainRunner {
 				System.out.println(chain5.weight);
 				System.out.println(chain5.stolen);
 				
-				Chain chain6=new Chain("Gold Chain",100000,14,"24 Carrot Gold",20, false,"Marriage");
+				Chain chain6=new Chain("ChainG",2000,14,"24 Carrot Gold",20, false,"Marriage");
 				System.out.println(chain6.type);
 				System.out.println(chain6.price);
 				System.out.println(chain6.length);
